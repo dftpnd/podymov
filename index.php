cc35674ed8f9e32700751ddaf44ca2a425692c8f
Gryaznov M.V <lkdnvc@gmail.com>
2013-10-16 15:48:15 +0400

    hello world

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-Сайт находится в разработке..
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru" lang="ru" >
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Подымов</title>
+</head>
+<body>
+    <h1>Сайт находится в разработке</h1>
+</body>
+</html>
